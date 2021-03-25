a29bd044c1750fe2999bd631429795c45d18df9d
gauravsingh5 <gauravsinghsisodia5@gmail.com>
2021-03-26 03:09:54 +0530

    Added mobile phone support

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
 export default function Header() {
   return (
     <>
-      <div className="full-div">
-        <p className="title">Scroll Down for random user profiles.</p>
+      <div className="bg-green-200 flex items-center justify-center h-screen">
+        <div className="text-center text-7xl font-mono font-semibold">
+          Scroll down for random user profiles.
+        </div>
       </div>
       <div>
         <br />
--- components/User.tsx
+++ components/User.tsx
@@ -1,4 +1,5 @@
 import { UserInfo, getUsername, getAddress } from "../utils/userData";
+import { BrowserView, MobileView } from "react-device-detect";
 
 interface UserProps {
   user: UserInfo;
@@ -7,12 +8,49 @@ interface UserProps {
 export default function User(props: UserProps): JSX.Element {
   return (
     <>
-      <div className="bg-green-200 p-6 shadow-xl flex m-3 space-x-4 rounded-lg">
-        <div className="flex-shrink-0">
-          <img src={props.user.picture.large} className="rounded-full w-48" />
+      <BrowserView>
+        <div className="bg-green-200 p-6 shadow-xl flex m-3 space-x-4 rounded-lg">
+          <div className="flex-shrink-0">
+            <img src={props.user.picture.large} className="rounded-full w-48" />
+          </div>
+          <div>
+            <div className="text-3xl font-mono font-semibold">
+              {getUsername(props.user)}
+            </div>
+            <div className="flex m-3 space-x-1">
+              <div className="flex font-mono">Email:</div>
+              <div className="flex font-mono text-gray-500">
+                {props.user.email}
+              </div>
+            </div>
+            <div className="flex m-3 space-x-1">
+              <div className="flex font-mono">Phone:</div>
+              <div className="flex font-mono text-gray-500">
+                {props.user.phone}
+              </div>
+            </div>
+            <div className="flex m-3 space-x-1">
+              <div className="flex font-mono">Age:</div>
+              <div className="flex font-mono text-gray-500">
+                {props.user.dob.age}
+              </div>
+            </div>
+            <div className="flex m-3 space-x-1">
+              <div className="flex font-mono">Address:</div>
+              <div className="flex font-mono text-gray-500">
+                {getAddress(props.user)}
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <div className="text-3xl font-mono font-semibold">
+      </BrowserView>
+      <MobileView>
+        <div className="bg-green-200 p-2 shadow-xl flex flex-col m-3 space-y-1">
+          <img
+            src={props.user.picture.large}
+            className="rounded-full w-48 ml-16"
+          />
+          <div className="text-3xl font-mono font-semibold self-center">
             {getUsername(props.user)}
           </div>
           <div className="flex m-3 space-x-1">
@@ -40,7 +78,7 @@ export default function User(props: UserProps): JSX.Element {
             </div>
           </div>
         </div>
-      </div>
+      </MobileView>
     </>
   );
 }
