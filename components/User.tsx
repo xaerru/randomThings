07b514c4d30ef33df326892507c8c7ad74ece1d3
gauravsingh5 <gauravsinghsisodia5@gmail.com>
2021-03-27 01:54:04 +0530

    Implemented UserData component

--- components/User.tsx
+++ components/User.tsx
@@ -1,5 +1,6 @@
 import { UserInfo, getUsername, getAddress } from "../utils/userData";
 import { BrowserView, MobileView } from "react-device-detect";
+import UserData from "./UserData";
 
 interface UserProps {
   user: UserInfo;
@@ -17,30 +18,10 @@ export default function User(props: UserProps): JSX.Element {
             <div className="text-3xl font-mono font-semibold">
               {getUsername(props.user)}
             </div>
-            <div className="flex m-3 space-x-1">
-              <div className="flex font-mono">Email:</div>
-              <div className="flex font-mono text-gray-500">
-                {props.user.email}
-              </div>
-            </div>
-            <div className="flex m-3 space-x-1">
-              <div className="flex font-mono">Phone:</div>
-              <div className="flex font-mono text-gray-500">
-                {props.user.phone}
-              </div>
-            </div>
-            <div className="flex m-3 space-x-1">
-              <div className="flex font-mono">Age:</div>
-              <div className="flex font-mono text-gray-500">
-                {props.user.dob.age}
-              </div>
-            </div>
-            <div className="flex m-3 space-x-1">
-              <div className="flex font-mono">Address:</div>
-              <div className="flex font-mono text-gray-500">
-                {getAddress(props.user)}
-              </div>
-            </div>
+            <UserData title="Email:" content={props.user.email} />
+            <UserData title="Phone:" content={props.user.phone} />
+            <UserData title="Age:" content={props.user.dob.age} />
+            <UserData title="Address" content={getAddress(props.user)} />
           </div>
         </div>
       </BrowserView>
@@ -53,30 +34,10 @@ export default function User(props: UserProps): JSX.Element {
           <div className="text-3xl font-mono font-semibold self-center">
             {getUsername(props.user)}
           </div>
-          <div className="flex m-3 space-x-1">
-            <div className="flex font-mono">Email:</div>
-            <div className="flex font-mono text-gray-500">
-              {props.user.email}
-            </div>
-          </div>
-          <div className="flex m-3 space-x-1">
-            <div className="flex font-mono">Phone:</div>
-            <div className="flex font-mono text-gray-500">
-              {props.user.phone}
-            </div>
-          </div>
-          <div className="flex m-3 space-x-1">
-            <div className="flex font-mono">Age:</div>
-            <div className="flex font-mono text-gray-500">
-              {props.user.dob.age}
-            </div>
-          </div>
-          <div className="flex m-3 space-x-1">
-            <div className="flex font-mono">Address:</div>
-            <div className="flex font-mono text-gray-500">
-              {getAddress(props.user)}
-            </div>
-          </div>
+          <UserData title="Email:" content={props.user.email} />
+          <UserData title="Phone:" content={props.user.phone} />
+          <UserData title="Age:" content={props.user.dob.age} />
+          <UserData title="Address" content={getAddress(props.user)} />
         </div>
       </MobileView>
     </>
