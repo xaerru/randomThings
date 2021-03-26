e3a11a09b2b46ebf8c1fbcb4740f456c789ae1ba
gauravsingh5 <gauravsinghsisodia5@gmail.com>
2021-03-27 01:40:50 +0530

    Added new component

---
+++ components/UserData.tsx
@@ -0,0 +1,13 @@
+interface UserDataProps {
+  title: string;
+  content: string;
+}
+
+export default function UserData(props: UserDataProps): JSX.Element {
+  return (
+    <div className="flex m-3 space-x-1">
+      <div className="flex font-mono">{props.title}</div>
+      <div className="flex font-mono text-gray-500">{props.content}</div>
+    </div>
+  );
+}
