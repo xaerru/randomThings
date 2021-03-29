488c834841eb45b23c4e1e8af2748661d261b500
gauravsingh5 <gauravsinghsisodia5@gmail.com>
2021-03-30 01:46:42 +0530

    Refactor code

--- components/User.tsx
+++ components/User.tsx
@@ -1,44 +1,36 @@
 import { BrowserView, MobileView } from "react-device-detect";
 import { getUsername, getAddress } from "../utils/userData";
-import { UserInfo } from "../utils/userInterface";
 import UserData from "./UserData";
 
-interface UserProps {
-  user: UserInfo;
-}
-
-export default function User(props: UserProps): JSX.Element {
+export default function User({ user }): JSX.Element {
   return (
     <>
       <BrowserView>
         <div className="bg-green-200 p-6 shadow-xl flex m-3 space-x-4 rounded-lg">
           <div className="flex-shrink-0">
-            <img src={props.user.picture.large} className="rounded-full w-48" />
+            <img src={user.picture.large} className="rounded-full w-48" />
           </div>
           <div>
             <div className="text-3xl font-mono font-semibold">
-              {getUsername(props.user)}
+              {getUsername(user)}
             </div>
-            <UserData title="Email:" content={props.user.email} />
-            <UserData title="Phone:" content={props.user.phone} />
-            <UserData title="Age:" content={props.user.dob.age} />
-            <UserData title="Address" content={getAddress(props.user)} />
+            <UserData title="Email:" content={user.email} />
+            <UserData title="Phone:" content={user.phone} />
+            <UserData title="Age:" content={user.dob.age} />
+            <UserData title="Address" content={getAddress(user)} />
           </div>
         </div>
       </BrowserView>
       <MobileView>
         <div className="bg-green-200 p-2 shadow-xl flex flex-col m-3 space-y-1">
-          <img
-            src={props.user.picture.large}
-            className="rounded-full self-center"
-          />
+          <img src={user.picture.large} className="rounded-full self-center" />
           <div className="text-3xl font-mono font-semibold self-center">
-            {getUsername(props.user)}
+            {getUsername(user)}
           </div>
-          <UserData title="Email:" content={props.user.email} />
-          <UserData title="Phone:" content={props.user.phone} />
-          <UserData title="Age:" content={props.user.dob.age} />
-          <UserData title="Address" content={getAddress(props.user)} />
+          <UserData title="Email:" content={user.email} />
+          <UserData title="Phone:" content={user.phone} />
+          <UserData title="Age:" content={user.dob.age} />
+          <UserData title="Address" content={getAddress(user)} />
         </div>
       </MobileView>
     </>
