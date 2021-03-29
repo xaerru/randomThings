84bb8a8fbd0f9e3b8d6a9ad72cd60a1486af670d
gauravsingh5 <gauravsinghsisodia5@gmail.com>
2021-03-29 13:15:56 +0530

    Remove unused bootstrap import

--- components/App.tsx
+++ components/App.tsx
@@ -19,12 +19,6 @@ export default function App(): JSX.Element {
     <div>
       <Head>
         <title>Random Users</title>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-          crossOrigin="anonymous"
-        />
       </Head>
       <Header />
       <InfiniteScroll
