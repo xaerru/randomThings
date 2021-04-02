4bd249a053f65fd06fb510dd63024fff5139acff
gauravsingh5 <gauravsinghsisodia5@gmail.com>
2021-04-03 02:14:06 +0530

    Use object pattern matching

--- components/UserDataItem.tsx
+++ components/UserDataItem.tsx
@@ -1,13 +1,8 @@
-interface UserDataItemProps {
-  title: string;
-  content: string;
-}
-
-export const UserDataItem = (props: UserDataItemProps): JSX.Element => {
+export const UserDataItem = ({ title, content }): JSX.Element => {
   return (
     <div className="flex m-3 space-x-1">
-      <div className="flex font-mono">{props.title}</div>
-      <div className="flex font-mono text-gray-500">{props.content}</div>
+      <div className="flex font-mono">{title}</div>
+      <div className="flex font-mono text-gray-500">{content}</div>
     </div>
   );
 };
