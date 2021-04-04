397fabbf7ae6ac9de17b041a0f3c4132580dcf84
gauravsingh5 <gauravsinghsisodia5@gmail.com>
2021-04-04 22:51:47 +0530

    Add new homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from "next/head";
+import { Card } from "../components/Card";
+
 export default function Home(): JSX.Element {
   return (
     <>
-      <h1>hello world</h1>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className="bg-green-200 flex flex-wrap justify-center items-center h-screen space-x-20">
+        <Card text="Random Users" href="/users" />
+      </div>
     </>
   );
 }
