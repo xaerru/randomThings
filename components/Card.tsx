4686feac547722202a9670ba2cdb14e481c55edc
gauravsingh <gauravsinghsisodia5@gmail.com>
2021-04-09 21:24:36 +0530

    Change styling

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,7 +9,7 @@ export const Card = ({ text, href }: CardProps): JSX.Element => {
   return (
     <>
       <Link href="/users">
-        <div className="card bg-green-300 flex flex-col justify-between w-72 sm:w-96 h-96 bg-center text-gray-800 shadow-md overflow-hidden cursor-pointer rounded-lg">
+        <div className="card bg-green-300 flex flex-col justify-between w-72 sm:w-96 h-72 sm:h-96 text-gray-800 shadow-lg rounded-lg">
           <div className="flex items-center justify-center h-screen">
             <div className="text-center text-7xl font-mono font-semibold">
               {text}
