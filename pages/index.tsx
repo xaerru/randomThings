da3d1a5122f4e9452021ff2b1da6e07514e99924
gauravsingh <gauravsinghsisodia5@gmail.com>
2021-04-16 20:46:11 +0530

    Revert anonymous function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Card } from "../components/Card";
 
-export default (): JSX.Element => {
+export default function Home(): JSX.Element {
   return (
     <>
       <Head>
@@ -19,4 +19,4 @@ export default (): JSX.Element => {
       </div>
     </>
   );
-};
+}
