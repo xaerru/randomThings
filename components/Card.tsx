1ad93f4fa66828305f43e04bac635c5fb857d57d
gauravsingh <gauravsinghsisodia5@gmail.com>
2021-04-29 17:01:47 +0530

    Reorder classes

--- components/Card.tsx
+++ components/Card.tsx
@@ -11,7 +11,7 @@ export const Card = ({ text, href }: CardProps): JSX.Element => {
       <Link href={href}>
         <div className="card bg-green-300 flex flex-col justify-between w-72 sm:w-96 h-72 sm:h-96 text-gray-800 shadow-lg rounded-lg">
           <div className="flex items-center justify-center h-screen">
-            <div className="text-center text-7xl font-mono font-semibold">
+            <div className="font-mono font-semibold text-center text-7xl">
               {text}
             </div>
           </div>
